0b6a5c5b65c7fa7adb92a3b0b7fde2b02a3a5c0c
black-knight <dani_ande@yahoo.dk>
2012-05-09 23:59:39 +0200

    Generic plugin methods

---
+++ src/java/com/trollsahead/qcumberless/plugins/ButtonBarMethodCallback.java
@@ -0,0 +1,26 @@
+// This file is part of Q-Cumberless Testing.
+//
+// Q-Cumberless Testing is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Q-Cumberless Testing is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Q-Cumberless Testing.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright 2012
+
+// Daniel Andersen (dani_ande@yahoo.dk)
+
+package com.trollsahead.qcumberless.plugins;
+
+import com.trollsahead.qcumberless.gui.Element;
+
+public interface ButtonBarMethodCallback {
+    void onTrigger(Element element);
+}
---
+++ src/java/com/trollsahead/qcumberless/plugins/generic/GenericDeviceSettings.java
@@ -0,0 +1,11 @@
+package com.trollsahead.qcumberless.plugins.generic;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: daniel
+ * Date: 5/9/12
+ * Time: 11:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GenericDeviceSettings {
+}
