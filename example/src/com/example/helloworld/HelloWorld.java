9cd1a66229b307ae944717974780b4d2dad28438
Daniel Andersen <dani_ande@yahoo.dk>
2012-05-22 21:00:50 +0200

    Android example improvement

--- example/src/com/example/helloworld/HelloWorld.java
+++ example/src/com/example/helloworld/HelloWorld.java
@@ -3,12 +3,17 @@ package com.example.helloworld;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.View;
+
+import java.lang.Readable;
 
 public class HelloWorld extends Activity {
    public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
-       TextView textView = new TextView(this);
-       textView.setText("Hello world");
-       setContentView(textView);
+       setContentView(R.layout.main);
    }
+
+    public void sayHello(View button) {
+        ((TextView)findViewById(R.id.HelloWorldTextView)).setText("Hello World!");
+    }
 }
